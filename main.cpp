e3fabe62553f2cdd9085633def1bfe16ad881c03
Makino0327 <k024g0008@g.neec.ac.jp>
2025-04-18 12:52:00 +0900

    初期化

--- main.cpp
+++ main.cpp
@@ -1,62 +1,8 @@
-#include <Windows.h>
-#include <d3d12.h>  
-#include <dxgi1_6.h>  
-#include <cassert>  
-#include <iostream> 
-#include <format>   
-#pragma comment(lib,"d3d12.lib")  
-#pragma comment(lib,"dxgi.lib")  
-
-void Log(const std::wstring& message) {
-    OutputDebugStringW(message.c_str());
-    OutputDebugStringW(L"\n");
-}
+#include <Windows.h>  
 
 
 // Windowsアプリでのエントリーポイント(main関数)  
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {  
-   // DXGIファクトリーの生成  
-   IDXGIFactory7* dxgiFactory = nullptr;  
-   // HRESULTはWindows系のエラーコードであり、  
-   // 関数が成功したかどうかをSUCCEEDEDマクロで判定できる  
-   HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));  
-   assert(SUCCEEDED(hr));  
-
-   IDXGIAdapter4* useAdapter = nullptr;  
-
-   for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i) {  
-       DXGI_ADAPTER_DESC3 adapterDesc{};  
-       hr = useAdapter->GetDesc3(&adapterDesc);  
-       assert(SUCCEEDED(hr));  
-       if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
-           Log(std::format(L"Use Adapter: {}\n", std::wstring(adapterDesc.Description)));
-           break;
-       }
-
-       useAdapter = nullptr;  
-   }  
-   assert(useAdapter != nullptr);  
-
-   ID3D12Device* device = nullptr;
-
-   D3D_FEATURE_LEVEL featureLevels[] = {
-       D3D_FEATURE_LEVEL_12_2,
-       D3D_FEATURE_LEVEL_12_1,
-       D3D_FEATURE_LEVEL_12_0,
-   };
-   const wchar_t* featureLevelStrings[] = { L"12.2", L"12.1", L"12.0" };
-
-
-   for (size_t i = 0; i < _countof(featureLevels); ++i) {
-	   hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
-	   if (SUCCEEDED(hr)) {
-		   Log(std::format(L"FeatureLevel: {}\n", featureLevelStrings[i]));
-		   break;
-	   }
-   }
-   assert(device != nullptr);
-   Log(std::wstring(L"Complete create D3D12Device!!!\n"));
-
 
    return 0;  
 }
