537021628618e74210893dc78bfe701f8523cd9a
Makino0327 <k024g0008@g.neec.ac.jp>
2025-04-19 23:54:18 +0900

    ウィンドウ作成前

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
 #include <Windows.h>  
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert> 
+#include <cstdint>
 
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+const int32_t kClientWidth = 1280;
+const int32_t kClientHeight = 720;
+
+ID3D12CommandQueue* commandQueue = nullptr;
+ID3D12Device* device = nullptr;
+ID3D12CommandAllocator* commandAllocator = nullptr;
+ID3D12GraphicsCommandList* commandList = nullptr;
+IDXGIFactory7* dxgiFactory = nullptr;
 
 // Windowsアプリでのエントリーポイント(main関数)  
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {  
+	// コマンドキュー作成
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
+	HRESULT hr = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
+	assert(SUCCEEDED(hr));
+	// コマンドアロケータを生成
+	hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
+	assert(SUCCEEDED(hr));
+	// コマンドリストを生成
+	hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList));
+	assert(SUCCEEDED(hr));
+
+	// スワップチェーンを生成
+	IDXGISwapChain4* swapChain = nullptr;
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	swapChainDesc.Width = kClientWidth;
+	swapChainDesc.Height = kClientHeight;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+
+	hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
+	assert(SUCCEEDED(hr));
 
    return 0;  
 }
