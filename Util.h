f2ab5aae579c5c640f377ff2b5bd07545b6cb256
Makino0327 <k024g0008@g.neec.ac.jp>
2025-10-20 15:13:02 +0900

    a

--- Util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-// 文字列変換(std::string -> std::wstring)
-std::wstring ConvertString(const std::string& str);
-
-// デバッグログ出力
-void Log(const std::wstring& message);
-
