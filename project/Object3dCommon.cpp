cc6e827f48f672be7df9b628270ffcc2ea3cb39c
Makino0327 <k024g0008@g.neec.ac.jp>
2025-11-24 17:30:14 +0900

    2値抜きができていない状態objは表示できている

--- project/Object3dCommon.cpp
+++ project/Object3dCommon.cpp
@@ -134,7 +134,7 @@ void Object3dCommon::CreateGraphicsPipelineState()
 
     // --- ラスタライザ ---
     D3D12_RASTERIZER_DESC rasterizerDesc{};
-    rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK; // ←3D は裏面カリング ON
+    rasterizerDesc.CullMode = D3D12_CULL_MODE_NONE; // ←3D は裏面カリング ON
     rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
     // --- シェーダをロード ---
